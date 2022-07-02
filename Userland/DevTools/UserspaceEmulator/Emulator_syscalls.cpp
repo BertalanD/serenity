ab25621d9501b26e37df24f0bfa82a3324fcfcf1
Daniel Bertalan <dani@danielbertalan.dev>
2022-07-02 22:34:42 +0200

    Kernel+LibC+LibCore: Implement `readlinkat(2)`

--- Userland/DevTools/UserspaceEmulator/Emulator_syscalls.cpp
+++ Userland/DevTools/UserspaceEmulator/Emulator_syscalls.cpp
@@ -1583,6 +1583,7 @@ int Emulator::virt$readlink(FlatPtr params_addr)
     Syscall::SC_readlink_params host_params;
     host_params.path = { (char const*)path.data(), path.size() };
     host_params.buffer = { (char*)host_buffer.data(), host_buffer.size() };
+    host_params.dirfd = params.dirfd;
     int rc = syscall(SC_readlink, &host_params);
     if (rc < 0)
         return rc;
